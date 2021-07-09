241c3a75549b78389bff6ba78cd8844c3dda9c7c
Shing <shing19@outlook.com>
2021-07-09 11:25:02 +0800

    merge server and local files

--- app.py
+++ app.py
@@ -2,10 +2,47 @@ import json,time
 from camera import VideoCamera
 from flask import Flask, render_template, request, jsonify, Response
 import requests
-import base64,cv2
-
+import hashlib
+import os
 
 app=Flask(__name__)
+
+# Jiaxin setting
+PICTURE_PATH = "static/images/"
+ALLOWED_EXTENSIONS = {'png', "PNG", 'jpg', "JPG", 'jpeg', "JPEG", 'gif', "GIF", }
+CHANGE_EXTENSIONS = {
+    'png': 'png',
+    'PNG': 'png',
+    'jpg': 'jpg',
+    'JPG': 'jpg',
+    'jpeg': 'jpg',
+    'JPEG': 'jpg'
+}
+
+
+@app.route("/upload_image", methods=["POST"])
+def add_picture():
+    img = request.files['image']
+    if img and '.' in img.filename:
+        ext = img.filename.rsplit('.', 1)[1]
+        if ext in ALLOWED_EXTENSIONS:
+            ext = CHANGE_EXTENSIONS[ext]
+            md5 = hashlib.md5(img.read()).hexdigest()
+            filename = md5 + '.' + ext
+            directory = os.path.join(PICTURE_PATH, filename)
+            if not os.path.isfile(directory):
+                img.seek(0)
+                img.save(directory)
+            return {"filename": filename}
+    return {"code": 401}
+
+
+@app.route("/hello", methods=["GET", "POST"])
+def hello():
+    return "hello"
+
+
+# default setting
 output=[]#("message stark","hi")]
 @app.route('/')
 def home_page():
@@ -81,7 +118,7 @@ def Result():
         return render_template("IY_Home_page.html",result=output)
 
 if __name__=="__main__":
-    app.run(debug=True)#,host="192.168.43.161")
+    app.run(debug=True)#,host="1.117.208.226")
 
 
 
