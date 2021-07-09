0b72d44c2d8242974aa1cffdd61588d69a3fe922
Jasonix <ccjjxx99@qq.com>
2021-07-09 13:50:51 +0800

    merge

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json,time
-from camera import VideoCamera
 from flask import Flask, render_template, request, jsonify, Response
 import requests
 import hashlib
@@ -39,7 +38,7 @@ def add_picture():
 
 @app.route("/hello", methods=["GET", "POST"])
 def hello():
-    return "hello"
+    return "hello merged"
 
 
 # default setting
@@ -60,41 +59,6 @@ def charts_page():
 @app.route('/cam')
 def sign_page():
     return render_template("camera.html")
-
-
-@app.route('/camera',methods=['POST'])
-def camera():
-    cap=cv2.VideoCapture(0)
-    while True:
-        ret,img=cap.read()
-        img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("static/cam.png",img)
-
-        # return render_template("camera.html",result=)
-        time.sleep(0.1)
-        return json.dumps({'status': 'OK', 'result': "static/cam.png"})
-        if cv2.waitKey(0) & 0xFF ==ord('q'):
-            break
-    cap.release()
-    # file="/home/ashish/Downloads/THOUGHT.png"
-    # with open(file,'rb') as file:
-    #     image=base64.encodebytes(file.read())
-    #     print(type(image))
-    # return json.dumps({'status': 'OK', 'user': user, 'pass': password});
-    return json.dumps({'status': 'OK', 'result': "static/cam.png"});
-
-def gen(camera):
-    while True:
-        data= camera.get_frame()
-
-        frame=data[0]
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route('/result',methods=["POST","GET"])
 def Result():
