4147de50c8ed35ab661497ca739f7ee0c50a111a
Shing <shing19@outlook.com>
2021-07-17 22:26:59 +0800

    upload img

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import json,time
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify, Response, flash, redirect, url_for, send_from_directory
 import requests
 import hashlib
 import os
+from werkzeug.utils import secure_filename
+from werkzeug.middleware.shared_data import SharedDataMiddleware
 
-app=Flask(__name__)
 
-# Jiaxin setting
 PICTURE_PATH = "static/images/"
 ALLOWED_EXTENSIONS = {'png', "PNG", 'jpg', "JPG", 'jpeg', "JPEG", }
 CHANGE_EXTENSIONS = {
@@ -17,36 +17,48 @@ CHANGE_EXTENSIONS = {
     'jpeg': 'jpg',
     'JPEG': 'jpg'
 }
-
-@app.route("/upload_image", methods=["POST"])
-def add_picture():
-    img = request.files['image']
-    if img and '.' in img.filename:
-        ext = img.filename.rsplit('.', 1)[1]
-        if ext in ALLOWED_EXTENSIONS:
-            ext = CHANGE_EXTENSIONS[ext]
-            md5 = hashlib.md5(img.read()).hexdigest()
-            filename = md5 + '.' + ext
-            directory = os.path.join(PICTURE_PATH, filename)
-            if not os.path.isfile(directory):
-                img.seek(0)
-                img.save(directory)
-            return {"filename": filename}
-    return {"code": 401}
-
+app=Flask(__name__)
+app.config['PICTURE_PATH'] = PICTURE_PATH
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['PICTURE_PATH']
+})
 
 @app.route("/hello", methods=["GET", "POST"])
 def hello():
     return "hello update from shing's repo"
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['PICTURE_PATH'],
+                               filename)
 
-# default setting
-
-output=[]#("message stark","hi")]
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
     return render_template("index.html",result=output)
+def add_picture():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        img = request.files['image']
+        if img.filename == '':
+            flash('No selected file')
+        if img and '.' in img.filename:
+            ext = img.filename.rsplit('.', 1)[1]
+            if ext in ALLOWED_EXTENSIONS:
+                ext = CHANGE_EXTENSIONS[ext]
+                md5 = hashlib.md5(img.read()).hexdigest()
+                filename = md5 + '.' + ext
+                directory = os.path.join(PICTURE_PATH, filename)
+                if not os.path.isfile(directory):
+                    img.seek(0)
+                    img.save(directory)
+                    return redirect(url_for('uploaded_file', filename=filename))
+    return redirect(url_for('/', {'code':401}))
 
+output=[]#("message stark","hi")]
 @app.route('/result',methods=["POST","GET"])
 def Result():
     if request.method=="POST":
